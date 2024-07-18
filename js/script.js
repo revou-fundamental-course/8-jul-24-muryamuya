8bf613335a6f7b99858146fe5aae2e5a53f3cdde
muryamuya <16364531+muryamuya@users.noreply.github.com>
2024-07-18 20:48:17 +0700

    reformat, add comments

--- js/script.js
+++ js/script.js
@@ -3,45 +3,49 @@ function hitungBMI() { // function to change element based on calculation
     let weightBMI = document.getElementById("weightBMI").value;
     let ageBMI = document.getElementById("ageBMI").value;
     let heightBMI = document.getElementById("heightBMI").value;
-    let calculated = weightBMI / ((heightBMI / 100) ^ 2)
     if (ageBMI < 18) { // show message invalid calculation
         alert("Harus 18 tahun keatas agar perhitungan valid");
     }
     else { // show calculation
-        showElementClass("displayHasil");
-        document.getElementById("hasilBMI").innerHTML = calculated.toFixed(1).toString();
-        if (calculated.toFixed(1) < 18.5) {
-            document.getElementById("kategoriBMI").innerHTML = "Anda kekurangan berat badan";
-        }
-        else if (calculated.toFixed(1) < 25) {
-            document.getElementById("kategoriBMI").innerHTML = "Anda memiliki berat badan ideal";
-        }
-        else if (calculated.toFixed(1) < 30) {
-            document.getElementById("kategoriBMI").innerHTML = "Anda kelebihan berat badan";
-        }
-        else {
-            document.getElementById("kategoriBMI").innerHTML = "Anda tergolong Obesitas";
+        try {
+            let calculated = weightBMI / ((heightBMI / 100) ^ 2)
+            showElementClass("displayHasil");
+            document.getElementById("hasilBMI").innerHTML = calculated.toFixed(1).toString();
+            if (calculated.toFixed(1) < 18.5) {
+                document.getElementById("kategoriBMI").innerHTML = "Anda kekurangan berat badan";
+            }
+            else if (calculated.toFixed(1) < 25) {
+                document.getElementById("kategoriBMI").innerHTML = "Anda memiliki berat badan ideal";
+            }
+            else if (calculated.toFixed(1) < 30) {
+                document.getElementById("kategoriBMI").innerHTML = "Anda kelebihan berat badan";
+            }
+            else {
+                document.getElementById("kategoriBMI").innerHTML = "Anda tergolong Obesitas";
+            }
+        } catch (error) {
+            console.log(error);
         }
     }
 }
 
 function resetAll() { // clear all fields
     hideElementClass("displayHasil");
-    document.getElementById("hasilBMI").innerHTML = null;
+    document.getElementById("hasilBMI").innerHTML = null; // clear element
     document.getElementById("kategoriBMI").innerHTML = null;
 }
 
-function hideElementClass(className) {
+function hideElementClass(className) { // hide class css
     let elements = document.getElementsByClassName(className);
-    for (i = 0; i < elements.length; i++) {
+    for (let i = 0; i < elements.length; i++) {
         let element = elements[i];
         element.classList.add("hidden")
     }
 }
 
-function showElementClass(className) {
+function showElementClass(className) { // show class css
     let elements = document.getElementsByClassName(className);
-    for (i = 0; i < elements.length; i++) {
+    for (let i = 0; i < elements.length; i++) {
         let element = elements[i];
         element.classList.remove("hidden")
     }
